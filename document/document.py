dc16e5f62876a397f649e87ff255d3c3470fbd70
Matous Machacek <machacekmatous@gmail.com>
2014-01-17 00:18:58 +0100

    parsing documents using bs4

--- document/document.py
+++ document/document.py
@@ -1,7 +1,6 @@
 import os
-import re
 
-from textwrap import dedent
+from bs4 import BeautifulSoup
 
 from .token import Token
 
@@ -17,84 +16,32 @@ class Document(object):
     
     def parse_vertical_format(self, document_path)
         with open(document_path, 'r') as file:
+            soup = BeautifulSoup(file, "xml") 
 
-            # Strip the lines and filter out empty lines
-            f = filter(None, (line.strip() for line in file)))
+            # These shoud be present for sure
+            self.docid = soup.DOC.DOCID.text
+            self.docno = soup.DOC.DOCNO.text
+            self.date  = soup.DOC.DATE.text
 
-            opening_tag = "<DOC>"
-            closing_tag = "</DOC>"
+            # Geography might not be present
+            try:
+                self.geography = soup.DOC.GEOGRAPHY.text
+            except:
+                self.geography = None
 
-            # Initial <DOC> tag
-            if next(f) != opening_tag:
-                raise MalformedVerticalFormatException("Missing <DOC> tag at the first line")
+            # Parse the sections
+            self.title = self.parse_section(soup.DOC.TITLE)
+            self.heading = self.parse_section(soup.DOC.HEADING)
+            self.text = self.parse_section(soup.DOC.TEXT)
 
-            self.docid = self.parse_tag(f, "DOCID")
-            self.docno = self.parse_tag(f, "DOCNO")
-            self.date = self.parse_tag(f, "DATE")
-            self.geography = self.parse_tag(f, "GEOGRAPHY")
+    def parse_section(self, soup_object):
 
-            self.title = self.parse_section(f, "TITLE")
-            self.heading = self.parse_section(f, "HEADING")
-            self.text = self.parse_section(f, "TEXT")
+        # If the element is not present, return empty list
+        if soup_object is None:
+            return []
 
-            # Closing </DOC> tag
-            if next(f) != closing_tag:
-                raise MalformedVerticalFormatException("Expected </DOC> tag")
-
-    # Compile the regular expresison once
-    tag_re = re.compile(r"^<%s>(?P<value>.+)</%s>$" % tag)
-
-    def parse_tag(self, f, tag):
-        
-        line = next(f)
-        result = self.tag_re.match(line)
-        if result is not None:
-            return result.group('value')
-        else:
-            raise MalformedVerticalFormatException(
-                    "The line \"\" does not match one line element format"
-                    )
-
-    def parse_section(self, f, section):     
-        opening_tag = "<%s>" % section
-        closing_tag = "</%s>" % section
-
-        if next(f) != opening_tag:
-            raise MalformedVerticalFormatException("Expected opening tag %s" % opening_tag)
-
-        result = []
-
-        for line in iter(f, closing_tag):
-            result.append(Token(line))
-
-        return result
-
-    def __str__(self):
-        return dedent("""
-            DocID:      {docid} 
-            DocNo:      {docno}
-            date:       {date}
-            geography:  {geography}
-
-            title:
-                {title1}
-                {title2}
-                ...
-
-
-
-
-            """.format(
-                docid = self.docid,
-                docno = self.docno,
-                date = self.date,
-                geography = self.geography,
-            )
-    
-
-
-
-
-    class MalformedVerticalFormatException(Exception):
-        pass
-
+        lines = soup_object.text.splitlines()
+        lines = (line.strip() for line in lines)
+        lines = filter(None, lines)
+        tokens = map(Token, lines)
+        return list(tokens)
