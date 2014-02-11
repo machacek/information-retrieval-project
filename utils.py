28445d317bb8bd68a42ae4d4782976f8598604b7
Matous Machacek <machacekmatous@gmail.com>
2014-02-11 15:05:56 +0100

    refactored factory

--- utils.py
+++ utils.py
@@ -5,5 +5,7 @@ class Factory(object):
     def choices(self):
         return self.types.keys() 
 
-    def __call__(self, type_name, *args, **kwargs):
-        return self.types[type_name](*args, **kwargs)
+    def __call__(self, type_str):
+        type_name, config_str = type_str.split(':',2)
+        args = config_str.split(',')
+        return self.types[type_name](*args)
