032aa6688247ca11d0df060029053e1076ca8822
Matous Machacek <machacekmatous@gmail.com>
2014-02-11 22:19:50 +0100

    parsing arguments moved out from main script

---
+++ config.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+#
+# Std library imports
+#
+import argparse
+import sys
+
+def parse_args():
+    
+    parser = argparse.ArgumentParser(
+            description="Creates index from given list of documents and performs documents retrieval for given list of topics",
+            epilog="Author: Matous Machacek <machacekmatous@gmail.com>")
+
+    #
+    # Obligatory options
+    #
+    parser.add_argument("-q",
+            help="a file with a list of topic filenames",
+            required=True,
+            type=argparse.FileType('r'),
+            dest="topics_list") 
+    
+    parser.add_argument("-d",
+            help="a file with a list of document filenames",
+            required=True,
+            type=argparse.FileType('r'),
+            dest="documents_list")
+
+    parser.add_argument("-p", "--pathprefix",
+            help="path to which filenames are relative to",
+            default="")
+
+    parser.add_argument("-r",
+            help="a label identifying particular experiment run",
+            default="no_id",
+            dest="run_id")
+
+    parser.add_argument("-o",
+            help="an output file",
+            default=sys.stdout,
+            type=argparse.FileType('r'),
+            dest="documents_list")
+
+    #
+    # Custom options
+    #
+    parser.add_argument("-c", "--lowercase",
+            help="lowercasing",
+            default="no",
+            type=caser_factory,
+            dest="caser",
+            choices=caser_factory.choices())
+
+    parser.add_argument("-t", "--termclasses",
+            help="turning text into bag of words/terms (forms, stems, lemmas, classes)",
+            default="wordforms",
+            type=termclassifier_factory,
+            dest="termclassifier",
+            choices=termclassifier_factory.choices())
+
+    parser.add_argument("-s","--stopwords",
+            help="removing stopwords",
+            default="none",
+            type=stopwords_factory,
+            dest="stopwords")
+
+    return parser.parse_args()
+
+config = parse_args()
