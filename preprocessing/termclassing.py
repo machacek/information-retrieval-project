3908676841f980ac0d7453b826ba67b806c30f87
Matous Machacek <machacekmatous@gmail.com>
2014-02-11 20:42:06 +0100

    refactored termclassing

--- preprocessing/termclassing.py
+++ preprocessing/termclassing.py
@@ -1,28 +1,8 @@
 from utils import Factory
 
-from document import DocumentTokens
-from topic import TopicTokens
-
-class TermClassifier(object):
-
-    def convert_document_collection(self, collection):
-        return (DocumentTokens(document, self) for document in collection)
-
-    def convert_topic_collection(self, collection):
-        return (TopicTokens(topic, self) for topic in collection)
-
-    def convert_vert_list(self, list):
-        return [ self.convert_vert_item(item) for item in list ]
-
-class WordFormClassifier(TermClassifier):
-    def convert_vert_item(self, vert_item):
-        return vert_item.form
-
-class LemmaClassifier(TermClassifier):
-    def convert_vert_item(self, vert_item):
-        return vert_item.lemma
+from operator import attrgetter
 
 termclassifier_factory = Factory({
-    'wordforms' : WordFormClassifier,
-    'lemmas'    : LemmasClassifier,
+    'wordforms' : attrgetter('form'),
+    'lemmas'    : attrgetter('lemma'),
     })
