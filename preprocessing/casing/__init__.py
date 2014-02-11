1c715edf65f41f11bb8ca2b66f4d5278d61f8624
Matous Machacek <machacekmatous@gmail.com>
2014-02-11 14:40:52 +0100

    refactored lowercasing

--- preprocessing/casing/__init__.py
+++ preprocessing/casing/__init__.py
@@ -1,18 +1,6 @@
 from utils import Factory
 
-class Caser(object):
-    def case_iterable(self, iterable):
-        return [self.case_str(token) for token in iterable]
-
-class LowerCaser(Caser):
-    def case_str(self, str):
-        return str.lower()
-
-class OriginalCaser(Caser):
-    def case_str(self, str):
-        return str
-
-lowercaser_factory = Factory({
-        'yes' : LowerCaser,
-        'no' : OriginalCaser,
+caser_factory = Factory({
+        'yes' : lambda x: x.lower(),
+        'no' : lambda x: x,
     })
