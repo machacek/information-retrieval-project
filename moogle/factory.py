9de459403ccc3208323f0c2979ed9d114cbfc6d9
Matous Machacek <machacekmatous@gmail.com>
2014-02-12 11:03:38 +0100

    raising argparse exception in factory

--- moogle/factory.py
+++ moogle/factory.py
@@ -1,3 +1,5 @@
+import argparse
+
 class Factory(object):
     def __init__(self, types, init=True):
         self.types = types
@@ -10,16 +12,19 @@ class Factory(object):
         return "{" + ",".join(self.choices()) + "}"
 
     def __call__(self, type_str):
-        if ':' in type_str:
-            type_name, config_str = type_str.split(':',2)
-            args = config_str.split(',')
-            if self.init:
-                return self.types[type_name](*args)
+        try:
+            if ':' in type_str:
+                type_name, config_str = type_str.split(':',2)
+                args = config_str.split(',')
+                if self.init:
+                    return self.types[type_name](*args)
+                else:
+                    return self.types[type_name]
             else:
-                return self.types[type_name]
-        else:
-            type_name = type_str
-            if self.init:
-                return self.types[type_name]()
-            else:
-                return self.types[type_name]
+                type_name = type_str
+                if self.init:
+                    return self.types[type_name]()
+                else:
+                    return self.types[type_name]
+        except:
+            raise argparse.ArgumentTypeError("'%s' is invalid argument." % type_str)
