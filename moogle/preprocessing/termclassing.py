b49da1bab6cc22817a0c40dc97c360e6ba9730b7
Matous Machacek <machacekmatous@gmail.com>
2014-02-12 11:09:36 +0100

    added shortlemmas

--- moogle/preprocessing/termclassing.py
+++ moogle/preprocessing/termclassing.py
@@ -3,6 +3,7 @@ from factory import Factory
 from operator import attrgetter
 
 termclassifier_factory = Factory(init=False, types={
-    'forms' : attrgetter('form'),
-    'lemmas'    : attrgetter('lemma'),
+    'forms'         : attrgetter('form'),
+    'lemmas'        : attrgetter('lemma'),
+    'shortlemmas'   : lambda x: x.lemma.split('_')[0]
     })
