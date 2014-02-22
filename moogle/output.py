2b86495a64ccf2f7a46b5052c1658b44657651ee
Matous Machacek <machacekmatous@gmail.com>
2014-02-22 11:21:03 +0100

    fixing output

--- moogle/output.py
+++ moogle/output.py
@@ -1,5 +1,5 @@
 from config import config
 
-def output(result)
+def output(topic, result):
     for rank, (docid, score) in enumerate(result.most_common()):
         print(topic.num, 0, docid, rank, score, config.run_id, sep='\t', file=config.output_file)
